8c1b9c9711cf209d71498f5fae41c3bdfe1d46ce
robotcopper <95591405+robotcopper@users.noreply.github.com>
2025-05-14 15:11:01 +0200

    feat: add uros timed publisher template exemple

---
+++ uros_publisher_timed_template/publisher.cpp
@@ -0,0 +1,150 @@
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include <time.h>
+extern"C"{
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include "robot_msg/msg/motor_msgs.h"
+
+#include "pico_uart_transports.h"
+}
+
+
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
+rcl_publisher_t publisher;
+robot_msg__msg__MotorMsgs publisher_msg;
+
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state;
+
+rcl_timer_t timer;
+rcl_node_t node;
+rcl_allocator_t allocator;
+rclc_support_t support;
+rclc_executor_t executor;
+
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time){
+	publisher_msg.motor1 = 10;
+	publisher_msg.motor2 = 20;
+	rcl_ret_t ret = rcl_publish(&publisher, &publisher_msg, NULL);
+}
+
+bool pingAgent(){
+    // Wait for agent successful ping for 2 minutes.
+    const int timeout_ms = 100;
+    const uint8_t attempts = 1;
+
+    rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
+
+    if (ret != RCL_RET_OK){
+    	//gpio_put(LED_PIN, 0);
+    	return false;
+    } else {
+    	//gpio_put(LED_PIN, 1);
+    }
+    return true;
+}
+
+void createEntities(){
+	allocator = rcl_get_default_allocator();
+
+	rclc_support_init(&support, 0, NULL, &allocator);
+
+	rclc_node_init_default(&node, "pico_node", "", &support);
+
+	rclc_publisher_init_default(
+			&publisher,
+			&node,
+			ROSIDL_GET_MSG_TYPE_SUPPORT(robot_msg, msg, MotorMsgs),
+			"pico_publisher_topic");
+
+	rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(1000),
+		timer_callback);
+
+	const rosidl_message_type_support_t * type_support =
+	    ROSIDL_GET_MSG_TYPE_SUPPORT(robot_msg, msg, MotorMsgs);
+
+	rclc_executor_init(&executor, &support.context, 2, &allocator);
+	rclc_executor_add_timer(&executor, &timer);
+}
+
+void destroyEntities(){
+	rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+	(void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+	rcl_publisher_fini(&publisher, &node);
+	rcl_timer_fini(&timer);
+	rclc_executor_fini(&executor);
+	rcl_node_fini(&node);
+	rclc_support_fini(&support);
+}
+
+int main()
+{
+	stdio_init_all();
+
+    rmw_uros_set_custom_transport(
+		true,
+		NULL,
+		pico_serial_transport_open,
+		pico_serial_transport_close,
+		pico_serial_transport_write,
+		pico_serial_transport_read
+	);
+
+    bool pulse;
+    //gpio_init(LED_PIN);
+    //gpio_set_dir(LED_PIN, GPIO_OUT);
+    //gpio_put(LED_PIN, pulse);
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    //gpio_put(LED_PIN, 1);
+    //sleep_ms(500);
+    //gpio_put(LED_PIN, 0);
+
+    allocator = rcl_get_default_allocator();
+    state = WAITING_AGENT;
+
+    while (true){
+    	switch (state) {
+    	    case WAITING_AGENT:
+    	      state = pingAgent() ? AGENT_AVAILABLE : WAITING_AGENT;
+    	      break;
+    	    case AGENT_AVAILABLE:
+    	      createEntities();
+    	      state = AGENT_CONNECTED ;
+    	      break;
+    	    case AGENT_CONNECTED:
+    	      state = pingAgent() ? AGENT_CONNECTED : AGENT_DISCONNECTED;
+    	      if (state == AGENT_CONNECTED) {
+    	        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+    	      }
+    	      break;
+    	    case AGENT_DISCONNECTED:
+    	      destroyEntities();
+    	      state = WAITING_AGENT;
+    	      break;
+    	    default:
+    	      break;
+    	  }
+
+        pulse = ! pulse;
+        //gpio_put(LED_PIN, pulse);
+    }
+    return 0;
+}
